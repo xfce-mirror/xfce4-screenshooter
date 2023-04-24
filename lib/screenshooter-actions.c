8669e08d4eca92bb19c1c33111de64720f6f8722
Andre Miranda <andreldm@xfce.org>
2023-04-24 23:02:10 +0200

    Add support to AVIF (#109)

--- lib/screenshooter-actions.c
+++ lib/screenshooter-actions.c
@@ -183,6 +183,8 @@ action_idle (gpointer user_data)
         extension = g_strdup ("webp");
       else if (G_UNLIKELY (g_str_has_suffix (save_location, ".jxl")))
         extension = g_strdup ("jxl");
+      else if (G_UNLIKELY (g_str_has_suffix (save_location, ".avif")))
+        extension = g_strdup ("avif");
 
       if (extension)
         {
