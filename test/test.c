66e3553e3129d7eb5d1c45967f0f1b6a665c32e5
Andre Miranda <andreldm@xfce.org>
2024-06-19 21:10:57 +0200

    Add more tests

--- test/test.c
+++ test/test.c
@@ -1,14 +1,39 @@
 #include <gtk/gtk.h>
 
+gboolean decoration = FALSE;
+
+static GOptionEntry entries[] =
+{
+  {
+    "decoration", 'd', G_OPTION_FLAG_IN_MAIN, G_OPTION_ARG_NONE, &decoration,
+    "Show window decorations",
+    NULL
+  },
+  {
+    NULL, ' ', 0, 0, NULL,
+    NULL,
+    NULL
+  }
+};
+
 int main (int argc, char *argv[])
 {
   GtkWidget *window;
   GtkCssProvider *provider;
+  GError *error = NULL;
 
-  gtk_init (&argc, &argv);
+  if (!gtk_init_with_args (&argc, &argv, NULL, entries, NULL, &error))
+  {
+    if (error != NULL)
+      {
+        g_print ("%s\nFailed to parse arguments.\n", error->message);
+        g_error_free (error);
+        return EXIT_FAILURE;
+      }
+  }
 
   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_decorated (GTK_WINDOW (window), FALSE);
+  gtk_window_set_decorated (GTK_WINDOW (window), decoration);
   gtk_window_set_default_size (GTK_WINDOW (window), 300, 300);
 
   provider = gtk_css_provider_new ();
--- test/test.sh
+++ test/test.sh
@@ -11,6 +11,9 @@ NC='\033[0m' # No Color
 TEST_FAILED=0
 TEST_FAILED_MSG=""
 
+XFWM4_THEME=Basix # Border size top=24px bottom=5px left=5px right=5px
+XFWM4_THEME_BAK=$(xfconf-query -c xfwm4 -p /general/theme)
+
 assertEquals()
 {
     expected=$1
@@ -24,6 +27,17 @@ assertEquals()
         TEST_FAILED_MSG="${actual} is not ${expected}"
         TEST_FAILED=1;
     fi
+}
+
+openTestWindow()
+{
+    /tmp/xfce4-screenshooter-test "$@" & PID=$!
+    sleep 1
+}
+
+closeTestWindow()
+{
+    kill "$PID"
 }
 
 test()
@@ -48,24 +62,45 @@ setUp()
     if [ ! -e /tmp/xfce4-screenshooter-test ]; then
         gcc $(pkg-config --cflags gtk+-3.0) test.c -o /tmp/xfce4-screenshooter-test $(pkg-config --libs gtk+-3.0)
     fi
-    /tmp/xfce4-screenshooter-test & PID=$!
-    sleep 1
+    xfconf-query -c xfwm4 -p /general/theme -s $XFWM4_THEME
+    
 }
 
 tearDown()
 {
-    kill "$PID"
+    xfconf-query -c xfwm4 -p /general/theme -s $XFWM4_THEME_BAK
 }
 
 testScreenshotActiveWindow()
 {
+    openTestWindow
     ../src/xfce4-screenshooter -w -s /tmp/test.png
     assertEquals '300x300' $(identify -format '%wx%h' /tmp/test.png)
-    assertEquals 0 $(convert /tmp/test.png -format %c histogram:info:- | grep -v '#00FF00' | wc -l)
+    assertEquals 0 $(magick /tmp/test.png -format %c histogram:info:- | grep -v '#00FF00' | wc -l)
+    closeTestWindow
+}
+
+testScreenshotActiveWindowDecoration()
+{
+    openTestWindow --decoration
+    ../src/xfce4-screenshooter -w -s /tmp/test.png
+    assertEquals '310x329' $(identify -format '%wx%h' /tmp/test.png)
+    assertEquals 0 $(magick /tmp/test.png -crop 300x300+5+24 -format %c histogram:info:- | grep -v '#00FF00' | wc -l)
+    closeTestWindow
+}
+
+testScreenshotActiveWindowDecorationScale2x()
+{
+    GDK_SCALE=2 openTestWindow --decoration
+    ../src/xfce4-screenshooter -w -s /tmp/test.png
+    assertEquals '610x629' $(identify -format '%wx%h' /tmp/test.png)
+    assertEquals 0 $(magick /tmp/test.png -crop 300x300+5+24 -format %c histogram:info:- | grep -v '#00FF00' | wc -l)
+    closeTestWindow
 }
 
 testScreenshotRegion()
 {
+    openTestWindow
     ../src/xfce4-screenshooter -r -s /tmp/test.png &
     sleep 1
     xdotool mousemove 100 100
@@ -75,7 +110,8 @@ testScreenshotRegion()
     xdotool mouseup 1
     sleep 1
     assertEquals '300x300' $(identify -format '%wx%h' /tmp/test.png)
-    assertEquals 0 $(convert /tmp/test.png -format %c histogram:info:- | grep -v '#00FF00' | wc -l)
+    assertEquals 0 $(magick /tmp/test.png -format %c histogram:info:- | grep -v '#00FF00' | wc -l)
+    closeTestWindow
 }
 
 testScreenshotFullscreen()
@@ -86,6 +122,8 @@ testScreenshotFullscreen()
 
 setUp
 test testScreenshotActiveWindow
+test testScreenshotActiveWindowDecoration
+test testScreenshotActiveWindowDecorationScale2x
 test testScreenshotRegion
 test testScreenshotFullscreen
 tearDown
